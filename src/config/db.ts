9b9710e8aa3f5884bb81891276530a4dfa5fb143
Josue herazo <josuedh18@hotmail.es>
2026-01-24 01:27:35 +0100

    Carga de sistema de citas y modelos

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,20 +2,18 @@ import {Sequelize} from "sequelize-typescript"
 import dotenv from "dotenv"
 // import Client from "../models/Clients.models"
 // import Service from "../models/Date.models"
-import DateList from "../models/DateList.models"
-import Client from "../models/Clients.models"
-import Service from "../models/service.model"
+
 dotenv.config()
  
 // console.log(process.env.DATABASE_URL);
 const db = new Sequelize(process.env.DATABASE_URL!, {
     // EL ERROR ESTABA AQUÍ: Los corchetes después de la coma estaban mal puestos
-    models: [Service, Client, DateList], 
+    models: [__dirname + "/../models/**/*.ts"], 
     logging: false,
     dialectOptions: {
         ssl: {
             require: true,
-            rejectUnauthorized: false // Necesario para Render/PostgreSQL
+            rejectUnauthorized: false 
         }
     }
 });
