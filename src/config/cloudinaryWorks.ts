a3b26d22097e64e122c7fd0586400df386d78a6a
unknown <josueherazo25@gmail.com>
2026-04-06 01:00:20 +0200

    fix: remove .env from ventas repo

--- src/config/cloudinaryWorks.ts
+++ src/config/cloudinaryWorks.ts
@@ -4,14 +4,11 @@ import multer from 'multer';
 console.log("Cloudinary Key:", process.env.CLOUDINARY_API_KEY ? "OK" : "MISSING");
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-    
+    cloud_name: "dm5kiejzk",
+    api_key: "927388969168581",
+    api_secret: "9Cd0mySTGLUtZcB0y2SfjeVsuxES"
 });
-console.log("Cloudinary Key:", process.env.CLOUDINARY_API_KEY);
-console.log("Cloudinary Secret:", process.env.CLOUDINARY_API_SECRET?.slice(0, 5) + "...");
-console.log("Cloudinary Cloud:", process.env.CLOUDINARY_CLOUD_NAME);
+
 const storage = multer.memoryStorage();
 
 export const uploadWork = multer({ storage });
